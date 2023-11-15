a7af5bcd9f1f9d8305008096219b4454ead9a56a
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-11-15 09:37:50 -0300

    Update, showModal

--- App.tsx
+++ App.tsx
@@ -1,39 +1,140 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  Modal,
+  Alert
 } from 'react-native';
+import { Button } from 'react-native-paper';
 import FirstComponents from './components/FirstComponents';
 
 import SecondComponent from './components/SecondComponent';
-
+import ModalLogin from './components/ModalLogin';
+import { PaperProvider } from 'react-native-paper';
 function App(): JSX.Element {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
+
+  //useEffect
+
+  const printOnchangeText = () => {
+    console.log('Escribiendo en el TextInput')
+    setEmail('ricky_uc11@hotmail.com')
+  }
+
+  const login = () => {
+    if (email && password) {
+      console.log('Ingresando al sistema')
+      setModalVisible(!modalVisible)
+
+    } else {
+      console.log('Datos incompletos')
+    }
+  }
   return (
-    <SafeAreaView>
-      <Text>Hola mundo</Text>
-      <FirstComponents />
-      <SecondComponent />
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.containerEmail}>
+          <Text>Email</Text>
+          <TextInput
+            placeholder="Ingresar email"
+            placeholderTextColor={'#FF9933'}
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+        <View style={styles.containerEmail}>
+          <Text>Contraseña</Text>
+          <TextInput
+            placeholder="Ingresar password"
+            placeholderTextColor={'#FF9933'}
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        {/* <Button title='Ingresar' onPress={login}>
+
+        </Button> */}
+        {/* <Pressable style={{marginTop:30, marginLeft:30}} onLongPress={login}>
+        <Text>Login</Text>
+      </Pressable> */}
+        <Button icon="save" mode="contained" onPress={login}>
+          Press me
+        </Button>
+
+        <ModalLogin
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        ></ModalLogin>
+
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
     fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  containerEmail: {
+    marginTop: 30,
+    marginLeft: 20
   },
-  highlight: {
-    fontWeight: '700',
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
---
+++ components/ModalLogin.js
@@ -0,0 +1,87 @@
+import {View, Text, StyleSheet, Modal, Pressable} from 'react-native';
+import React from 'react';
+
+export default function ModalLogin(props) {
+  const {modalVisible, setModalVisible} = props;
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+        setModalVisible(!modalVisible);
+      }}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>
+            Ha entrado satisfactoriamente al sistema
+          </Text>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Text style={styles.textStyle}>Hide Modal</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600',
+  },
+  containerEmail: {
+    marginTop: 30,
+    marginLeft: 20,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
